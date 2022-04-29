cb80ee5dffd05efbab95e5831e0da0eac064e667
VenkateshNambiyar <venkatesh@thelaunchclub.in>
2022-04-29 13:02:22 +0530

    Change Scanner Name & Line Spacing for Logical Blocks

--- Travel.java
+++ Travel.java
@@ -3,7 +3,7 @@ package com.bus;
 import java.util.*;
 
 public class Travel {
-	private Scanner destination = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
 	
 	private int passengers;
 	private double amountPaid;
@@ -16,12 +16,14 @@ public class Travel {
 	private double totalPrice;
 	
 	private final void checkPassenger() {
+		
 		while (true) {
+			
 			try {
 				busticket();
 				break;
 			} catch (InputMismatchException e) {
-				destination.nextLine();
+				scanner.nextLine();
 			}
 		}
 	}
@@ -29,63 +31,10 @@ public class Travel {
 	private final void showSelection() {
 		System.out.println("\n   DESTINATION    1.)Tuticorin \t 2.)Madurai \t 3.)Nagercoil \t 4.)Tenkasi");
 		System.out.print("Select A Destination : \t\t ");
-		booking = destination.nextInt();
+		booking = scanner.nextInt();
 		
 		checkSelection();
-	}
-	
-	private final void checkSelection() {
-		if ((booking >= maximumDestinationLimit ) || (booking < minimumDestinationLimit)) {
-			throw new IncorrectDetailsException("Invalid Entry So Kindly Retry..");
-		}
-	}
-	
-	private final void passengerDetails() {
-		System.out.print( " Enter a passengersDetails :  \t");
-		passengers = destination.nextInt();
 		
-		validatePassenger();
-	}
-	
-	private final void validatePassenger() {
-		if (passengers < minimumPassengerLimit) {
-			throw new IncorrectDetailsException("Invalid Transacation So Kindly Retry..");
-		}
-	}
-	
-	private final void ticketAmount() {
-		System.out.print( " Enter a Base TicketPrice : \t " );
-		baseTicketPrice = destination.nextDouble();
-	}
-	
-	private final void showPayment() {
-		totalPrice = passengers * baseTicketPrice;
-		
-		System.out.println( " Total Price of the Ticket : \t " + totalPrice );
-	}
-	
-	private final void payment() {
-		System.out.print( " Passenger Amount Paid : \t " );
-		amountPaid = destination.nextDouble();
-	
-		validatePayment();
-	}
-	
-	private final void validatePayment() {
-		if (amountPaid < totalPrice) {
-			throw new IncorrectDetailsException("Invalid Transacation So Kindly Retry..\n");
-		}
-	}
-	
-	private final void billing() {
-		balance = amountPaid - totalPrice;
-		
-		System.out.println( " Passenger Balance Amount: \t " + balance );
-	}
-	
-	private final void busticket() throws RuntimeException {
-		showSelection();
-	
 		switch (booking) {
 		case 1:
 			System.out.println(" Bus No : 1212 Departs at 00.10 From Tirunelveli To Tuticorin ");
@@ -102,7 +51,62 @@ public class Travel {
 		default:
 			System.out.println("Thank YOu");
 		}
+	}
+	
+	private final void checkSelection() {
 		
+		if ((booking >= maximumDestinationLimit ) || (booking < minimumDestinationLimit)) {
+			throw new IncorrectDetailsException("Invalid Entry So Kindly Retry..");
+		}
+	}
+	
+	private final void passengerDetails() {
+		System.out.print( " Enter a passengersDetails :  \t");
+		passengers = scanner.nextInt();
+		
+		validatePassenger();
+	}
+	
+	private final void validatePassenger() {
+		
+		if (passengers < minimumPassengerLimit) {
+			throw new IncorrectDetailsException("Invalid Transacation So Kindly Retry..");
+		}
+	}
+	
+	private final void ticketAmount() {
+		System.out.print( " Enter a Base TicketPrice : \t " );
+		baseTicketPrice = scanner.nextDouble();
+	}
+	
+	private final void showPayment() {
+		totalPrice = passengers * baseTicketPrice;
+		
+		System.out.println( " Total Price of the Ticket : \t " + totalPrice );
+	}
+	
+	private final void payment() {
+		System.out.print( " Passenger Amount Paid : \t " );
+		amountPaid = scanner.nextDouble();
+	
+		validatePayment();
+	}
+	
+	private final void validatePayment() {
+		
+		if (amountPaid < totalPrice) {
+			throw new IncorrectDetailsException("Invalid Transacation So Kindly Retry..\n");
+		}
+	}
+	
+	private final void billing() {
+		balance = amountPaid - totalPrice;
+		
+		System.out.println( " Passenger Balance Amount: \t " + balance );
+	}
+	
+	private final void busticket() throws RuntimeException {
+		showSelection();
 		passengerDetails();
 		ticketAmount();
 		showPayment();
